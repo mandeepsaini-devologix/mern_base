41551044addbd7025d9838a7549ff20636922a48
Mandeep Saini <mandeep_saini@gmail.com>
2024-11-26 22:04:57 +0530

    Admin theme with DB

--- app/controllers/admin/dashboard.controller.js
+++ app/controllers/admin/dashboard.controller.js
@@ -12,15 +12,15 @@ const authMiddleware = require('../../middlewares/auth.middleware');
 const auth = require('@helpers/auth.helper');
 
 //authMiddleware('web','test:test1'),
-router.get('/',    (req, res) =>
+router.get('/', authMiddleware('web','test:test1'),  (req, res) =>
 {
 
   
-  //res.send('Dashboard ' + JSON.stringify(req.auth.status)  );
+  res.send('Dashboard ' + JSON.stringify(req.auth.status)  );
   
 
-  //res.render('admin/main',{ layout: 'admin/layouts/layout' });
-  res.render('admin/main',{ layout: 'admin/layouts/layout' });
+  // res.render('admin/main',{ layout: 'admin/layouts/layout' });
+  // res.render('admin/main',{ layout: 'admin/layouts/layout' });
 
 })
 
